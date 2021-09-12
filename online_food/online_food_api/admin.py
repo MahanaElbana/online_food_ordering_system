4364ff1bbd379d0bbd331ec52644461563592976
Mahney Elbana (TRIPLE M) <cbmbbmj@gmail.com>
2021-09-12 22:40:00 +0200

    add another tables to database and code of admin

--- online_food/online_food_api/admin.py
+++ online_food/online_food_api/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Menu ,Category ,Customer ,Restaurant ,Reating ,Reservation ,Order
 # Register your models here.
+admin.site.register(Menu)
+admin.site.register(Restaurant)
+admin.site.register(Category)
+admin.site.register(Customer)
+admin.site.register(Order)
+admin.site.register(Reservation)
+admin.site.register(Reating)
--- online_food/online_food_api/models.py
+++ online_food/online_food_api/models.py
@@ -3,15 +3,23 @@ from django.db import models
 # Create your models here.
 
 class Menu(models.Model):
-    rest_id = models.ForeignKey("Restaurant", on_delete=models.CASCADE)
+    rest_id = models.ForeignKey("Restaurant", on_delete=models.CASCADE ,related_name="menu_list")
     name = models.CharField(max_length=150)
     price = models.DecimalField(max_digits=5, decimal_places=2)
-    about = models.TextField()
+    about = models.TextField(max_length=300)
     image = models.ImageField(upload_to='menu/%Y/%m/%d/')
     
     def __str__(self):
         return self.name
 
+class Category(models.Model):
+    category_name = models.CharField(max_length=150)
+    description = models.TextField(max_length=300)
+    created =models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self) :
+        return self.category_name
+        
 class Restaurant(models.Model):
     name = models.CharField(max_length=150)
     email = models.EmailField(max_length=254)
@@ -21,6 +29,7 @@ class Restaurant(models.Model):
     close_hours = models.IntegerField()
     address = models.CharField(max_length=150)
     image = models.ImageField(upload_to='restaurent/%Y/%m/%d/')
+    cat_id = models.ForeignKey(Category , on_delete=models.CASCADE,related_name="category") 
 
     def __str__(self):
         return self.name
@@ -32,20 +41,25 @@ class Customer(models.Model):
     password = models.CharField(max_length=150)
     phone = models.CharField(max_length=15)
     address = models.CharField(max_length=150)
-    reservate = models.ManyToManyField('Order', through='Reservation')
+    #reservate = models.ManyToManyField('Order', through='Reservation')
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
-
-class Reservation(models.Model):
-    order_id = models.ForeignKey('Order', on_delete=models.CASCADE)
-    customer_id = models.ForeignKey(Customer, on_delete=models.CASCADE)
 
 class Order(models.Model):
     name = models.CharField(max_length=150)
     quantity = models.IntegerField()
     price = models.DecimalField(max_digits=5, decimal_places=2)
-    date = models.DateTimeField(auto_now_add=True)
-    # item : i don't what it mean
+    created = models.DateTimeField(auto_now_add=True)
+  
     def __str__(self):
         return self.name
+
+class Reservation(models.Model):
+    order_id = models.ForeignKey(Order, on_delete=models.CASCADE ,related_name="orders")
+    customer_id = models.ForeignKey(Customer, on_delete=models.CASCADE ,related_name="customers")
+
+class Reating(models.Model):
+    menu_id = models.ForeignKey(Menu, on_delete=models.CASCADE ,related_name="ord")
+    customer_id = models.ForeignKey(Customer, on_delete=models.CASCADE ,related_name="cust")
+
